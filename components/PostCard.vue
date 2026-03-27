4c80766ca321bfc32f06f024c6ae01df94bd24db
AniketKarse <aniketkarse712@gmail.com>
2026-03-27 10:45:17 +0000

    chore: postCard components

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-import type { Post } from '@/data/posts'
-import { getPostsUrl } from '@/data/posts'
 
-const { data: posts } = await useFetch<Post[]>(getPostsUrl())
+import type { Post } from '@/data/posts'
+const { post } = defineProps<{ post: Post }>()
+
 </script>
 
 <template>
-  <main>
-    <h1>Nuxt.js Examples</h1>
-    <div>
-      <PostCard 
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      />
+  <div class="card">
+		<h2 class="title">
+    {{ post.title }}
+		</h2>
+    <div class="category">
+      <a href="#">Category: {{ post.category.name }}</a>
     </div>
-  </main>
+    <RenderMarkdown :source="post.intro" />
+  </div>
 </template>
 
 <style scoped lang="scss">
